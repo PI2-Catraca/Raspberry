613e60695de2979cf1d8ee8c0bca21aebb98c07a
KalebeLopes <calbkalebe@gmail.com>
2022-08-10 14:12:41 -0300

    feat: adicionando endpoint para receber e salvar o arquivo pickle enviado pela API de treinamento

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from typing import List
 from fastapi import FastAPI
 import pickle
 from pydantic import BaseModel
 
 class Pickle(BaseModel):
-    imagem: str
+    encodings: List[List[float]]
+    names: List[str]
 
 app = FastAPI()
-
 @app.post("/api/encondings")
-async def teste(data: Pickle):
-    f = open("encodings.pickle", "wb")
-    f.write(data['imagem'])
+async def teste(imagem: Pickle):
+    objImagem = {
+        "encodings": imagem.encodings,
+        "names": imagem.names
+    }
+
+    with open("encodings.pickle", "wb") as p:
+        pickle.dump(objImagem, p)
+
+    return 'arquivo pickle atualizado'
